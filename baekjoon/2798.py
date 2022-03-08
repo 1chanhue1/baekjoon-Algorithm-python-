390f847be5adf203c5147fa7ad0cac287b9c5bd4
1chanhue1 <chanhue467@gmail.com>
2022-03-08 21:36:16 +0900

    블랙잭

--- baekjoon/2798.py
+++ baekjoon/2798.py
@@ -1,1 +1,23 @@
-print("hi")
+import math
+
+
+
+N,M=map(int,input().split())
+
+
+save=list(map(int,input().split()))
+
+result=0
+for i in range(N):
+  for j in range(i+1,N):
+    for k in range(j+1,N):
+      if(save[i]+save[j]+save[k]>M):
+        continue
+      else:
+        result=max(result,save[i]+save[j]+save[k])
+
+
+print(result)    
+
+
+
